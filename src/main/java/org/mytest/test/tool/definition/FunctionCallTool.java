b3cbcd980c332b0a458bf1c5cd6bdb4b8aba18aa
gemo <1151641026@qq.com>
2025-09-30 09:46:28 +0800

    工具定义

--- src/main/java/org/mytest/test/tool/definition/FunctionCallTool.java
+++ src/main/java/org/mytest/test/tool/definition/FunctionCallTool.java
@@ -1,9 +1,26 @@
 package org.mytest.test.tool.definition;
 
+import dev.langchain4j.agent.tool.ToolSpecifications;
 import dev.langchain4j.service.tool.DefaultToolExecutor;
 import lombok.Data;
+
+import java.lang.reflect.Method;
 
 @Data
 public class FunctionCallTool extends BaseTool{
     protected DefaultToolExecutor toolExecutor;
+
+    public FunctionCallTool(Object obj, String methodName) {
+        Class<?> aClass = obj.getClass();
+        Method[] methods = aClass.getDeclaredMethods();
+        Method fcMethod = null;
+        for (Method method : methods) {
+            if(method.getName().equals(methodName)){
+                fcMethod = method;
+                break;
+            }
+        }
+        toolSpecification = ToolSpecifications.toolSpecificationFrom(fcMethod);
+        toolExecutor = new DefaultToolExecutor(obj, fcMethod);
+    }
 }
--- src/main/java/org/mytest/test/tool/definition/McpTool.java
+++ src/main/java/org/mytest/test/tool/definition/McpTool.java
@@ -1,9 +1,15 @@
 package org.mytest.test.tool.definition;
 
+import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.mcp.client.McpClient;
 import lombok.Data;
 
 @Data
-public class McpTool extends BaseTool{
+public class McpTool extends BaseTool {
     protected McpClient mcpClient;
+
+    public McpTool(ToolSpecification toolSpecification, McpClient mcpClient) {
+        this.toolSpecification = toolSpecification;
+        this.mcpClient = mcpClient;
+    }
 }
